755dc9252e600060c49a47a29b216e6c6ce58401
SouthSide.73 <99437405+SouthSide73@users.noreply.github.com>
2022-09-24 15:00:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import numpy as np
 from numpy import linalg
 
-try:
-    m = int(input('Введите размерность квадратной матрицы больше 1 и меньше 31:'))
-    while (m < 1) or (m > 31):
-        m = int(input("Вы ввели неверное число. "
-                      "\nВведите количество строк (столбцов) квадратной матрицы больше 1 и меньше 31:"))
-    a = np.random.randint(5, size=(m, m))
-    r = np.linalg.matrix_rank(a)
-    print("Матрица:\n", a)
-    print("Ранг матрицы:", r)
-    t = int(input('Введите количество знаков после запятой в результате вычисления:'))
-    t = 0.1 ** t
-    n = 1
-    first = 1
-    second = 1
-    fact = 1
-    summa = 0
+m = int(input('Введите размерность квадратной матрицы больше 1 и меньше 31:'))
+while (m < 1) or (m > 31):
+    m = int(input("Вы ввели неверное число. "
+                  "\nВведите количество строк (столбцов) квадратной матрицы больше 1 и меньше 31:"))
+a = np.random.randint(5, size=(m, m))
+r = np.linalg.matrix_rank(a)
+print("Матрица:\n", a)
+print("Ранг матрицы:", r)
+t = int(input('Введите количество знаков после запятой в результате вычисления:'))
+t = 0.1 ** t
+n = 1
+first = 1
+second = 1
+fact = 1
+summa = 0
+fg = 0
+out = 1
+while abs(out) > t:
+    out = 0
+    fact = fact*(2*n - 1)*first*second
+    fg += summa
+    summa += (np.linalg.det(linalg.matrix_power(a, 2*n-1))) / fact
+    out = abs(fg-summa)
+    n += 1
+    second = second * 2
     fg = 0
-    out = 1
-    while abs(out) > t:
-        out = 0
-        fact = fact*(2*n - 1)*first*second
-        fg += summa
-        summa += (np.linalg.det(linalg.matrix_power(a, 2*n-1))) / fact
-        out = abs(fg-summa)
-        n += 1
-        second = second * 2
-        fg = 0
-        print(n-1, ':', summa, ' ', out)
-    print('Сумма знакопеременного ряда:', summa)
-except OverflowError:
-    print("\nПрограмма не может произвести дальнейшие вычисления")
-except RuntimeWarning:
-    print("\nПрограмма не может произвести дальнейшие вычисления")
+    print(n-1, ':', summa, ' ', out)
+print('Сумма знакопеременного ряда:', summa)
